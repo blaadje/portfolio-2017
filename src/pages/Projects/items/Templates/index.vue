5d3cb0edfe5c883e61c6f26c2bac40b790b33623
Alex <alexandre.charlot@wisembly.com>
2018-04-27 22:47:31 +0200

    refactoring projects

---
+++ src/pages/Projects/items/Templates/index.vue
@@ -0,0 +1,37 @@
+<style lang="sass" scoped src="./style.sass"></style>
+<script>
+export default {
+  props: {
+    title: { type: String },
+    description: { type: String },
+    tags: { type: Array },
+    github: { type: String },
+    facebook: { type: String }
+  }
+}
+</script>
+<template lang="pug">
+.overlay
+  .wrapper
+    .content
+      .title
+        h1 {{ title }}
+        p.
+          {{ description }}
+        .tags(v-for='item in tags')
+          span.button {{ item }}
+      .socialNetwork
+        a(:href="github").button
+          img(src="")
+          span Github
+        a(:href="facebook").button
+          img(src="")
+          span Facebook
+      a(href="").preview Preview
+      img(src="")
+      .browser
+        h2 browser compatibility
+        img(src="")
+        img(src="")
+
+</template>
---
+++ src/pages/Projects/items/index.vue
@@ -0,0 +1,23 @@
+<script>
+import Templates from './Templates'
+import datas from './Templates/projectItems'
+
+export default {
+  components: { Templates },
+  data () {
+    return {
+      datas
+    }
+  }
+}
+</script>
+<template>
+  <Templates
+    v-if="this.$route.name"
+    :title="datas[this.$route.name].title"
+    :description="datas[this.$route.name].description"
+    :tags="datas[this.$route.name].tags"
+    :github="datas[this.$route.name].github"
+    :facebook="datas[this.$route.name].facebook"
+  />
+</template>
