c85fe9f4601af717d5944f59fbf69ba5e8f303b1
blaadje <acharlot91@gmail.com>
2023-05-23 15:19:53 +0200

    add docs for github pages

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
 
 let mainConfig = merge(common, {
   mode: 'production',
+  output: {
+    filename: '[name].bundle.js',
+      path: path.resolve(__dirname, '../docs'),
+   },
 })
 
 module.exports = mainConfig
